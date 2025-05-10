304bac362ee11d3ef9aeeac291d27bda07e9c4f8
mwafg gumma <mwafggommaabdula@gmail.com>
2025-05-10 20:17:04 +0530

    add link to it

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"/Auto-Vibe/",
   server: {
     port: 8000,
   }
